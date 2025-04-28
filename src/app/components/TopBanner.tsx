f498b7bdc9d509206385339962ccb66ef0a4ad8b
Ukasha Manzoor <ukashamanzoor253@gmail.com>
2025-04-28 19:22:19 +0500

    stats and testomial chages in home page and made footer according to the design

--- src/app/components/TopBanner.tsx
+++ src/app/components/TopBanner.tsx
@@ -8,19 +8,19 @@ export const TopBanner = () => {
   if (!visible) return null;
 
   return (
-    <header className="flex flex-wrap gap-4 justify-between items-center py-3.5 px-4 md:px-20 w-full text-base font-medium text-center text-white bg-sky-600">
-      <p className="text-sm md:text-base text-center md:text-left w-full md:w-auto">
-        <strong className="font-black">Courses from $149.00</strong> Gain the
-        skills to climb that career ladder.{" "}
-        <span className="font-bold text-[#FFCA7E]">3 days left!</span>
-      </p>
-      <button onClick={() => setVisible(false)}>
-        <img
-          src="/img/cross.svg"
-          className="w-6 h-6"
-          alt="Close banner"
-        />
-      </button>
+    <header className="flex flex-wrap  justify-between items-center py-3.5  md:px-2 w-full text-base font-medium text-center text-white bg-sky-600">
+      <div className="w-11/12 flex flex-row items-center justify-center">
+        <p className="text-sm md:text-base text-center md:text-left w-full md:w-auto">
+          <strong className="font-black">Courses from $149.00</strong> Gain the
+          skills to climb that career ladder.{" "}
+          <span className="font-bold text-[#FFCA7E]">3 days left!</span>
+        </p>
+      </div>
+      <div className="">
+        <button onClick={() => setVisible(false)}>
+          <img src="/img/cross.svg" className="w-6 h-6" alt="Close banner" />
+        </button>
+      </div>
     </header>
   );
 };
