2c0b61fff79bab560f25537285529fb32ef3b0a2
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2025-04-30 01:31:10 +0500

    Refactor CourseDetailPage to handle async params for slug extraction
    - Updated the CourseDetailPage component to accept a Promise for the params, ensuring proper handling of the slug extraction.
    - Improved the asynchronous fetching of course details from the API based on the slug.

--- src/app/course/[slug]/page.tsx
+++ src/app/course/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import CourseDetailPageClient from "./page.client";
 import { api } from "@/app/utils/axios";
 
-const CourseDetailPage = async ({ params }: { params: { slug: string } }) => {
-  const { slug } = params;
+const CourseDetailPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const { slug } = await params;
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/courses/${slug}`, {
     next: {
