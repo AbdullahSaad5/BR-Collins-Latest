a819b9630634bb390e81e9f0a4bdf0f49d24f93c
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2025-04-30 21:07:04 +0500

    Remove deprecated TopBanner copy component and update TopBanner for improved layout and styling consistency

--- src/app/components/TopBanner.tsx
+++ src/app/components/TopBanner.tsx
@@ -9,10 +9,11 @@ export const TopBanner = () => {
 
   return (
     <header className="flex flex-wrap  justify-between items-center md:h-[50px]  md:px-2 w-full text-base font-medium text-center text-white bg-sky-600">
+      <div className="w-6"></div>
       <div className="w-11/12 flex flex-row items-center justify-center">
-        <p className="text-sm md:text-base text-center md:text-left w-full md:w-auto">Courses from
-          <strong className="font-black"> $149.00</strong> Gain the
-          skills to climb that career ladder.{" "}
+        <p className="text-sm md:text-base text-center md:text-left w-full md:w-auto">
+          Courses from
+          <strong className="font-black"> $149.00</strong> Gain the skills to climb that career ladder.{" "}
           <span className="font-bold text-[#FFCA7E]">3 days left!</span>
         </p>
       </div>
